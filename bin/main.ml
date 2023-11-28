ab70ebd1bba37d386dbc2bdf4d6b491798027cfc
Reynir Björnsson <reynir@reynir.dk>
2023-11-28 16:59:47 +0100

    Add --colonize option
    Some tools like to add or expect a bunch of colons between every pair of
    hexadecimal characters. The --colonize flag adds a colon so the output
    will look like:
    
        00:11:...:ee:ff

--- bin/main.ml
+++ bin/main.ml
@@ -16,12 +16,23 @@ let fp_of_cert_file cert_file =
   let pubkey = X509.Certificate.public_key cert in
   X509.Public_key.fingerprint pubkey
 
-let jump cert_files =
+let jump colonize cert_files =
   let cert_files = List.map Fpath.v cert_files in
   let fps = List.map fp_of_cert_file cert_files in
+  let pp_hex =
+    if not colonize then
+      Hex.pp
+    else
+      fun ppf (`Hex hex) ->
+        if String.length hex >= 2 then
+          Fmt.pf ppf "%c%c" hex.[0] hex.[1];
+        for i = 1 to (String.length hex - 1) / 2 do
+          Fmt.pf ppf ":%c%c" hex.[2*i] hex.[2*i+1]
+        done
+  in
   List.iter2
     (fun cert_file fp ->
-       Fmt.pr "%a: %a\n" Fpath.pp cert_file Hex.pp (Hex.of_cstruct fp))
+       Fmt.pr "%a: %a\n" Fpath.pp cert_file pp_hex (Hex.of_cstruct fp))
     cert_files
     fps
 
@@ -31,13 +42,18 @@ let certs =
   let doc = "PEM-encoded certificate file" in
   Arg.(value & pos_all non_dir_file [] & info ~docv:"CERT_FILE" ~doc [])
 
+let colonize =
+  let doc = "Add a bunch of colons in the output. \
+             This has nothing to do with colonialism." in
+  Arg.(value & flag & info ~doc [ "colonize" ])
+
 let cmd =
   let info =
     Cmd.info  "cert-pubkey-fp"
       ~version:"%%VERSION%%"
       ~doc:"Print public key fingerprints of certificates"
   in
-  Cmd.v info Term.(const jump $ certs)
+  Cmd.v info Term.(const jump $ colonize $ certs)
 
 let () =
   exit @@
